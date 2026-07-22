0348d12aef365fb507ffecc294fa299e28674c59
Mishka Hagelberg <mishka.hagelberg@gmail.com>
2026-07-22 13:36:57 +0300

    2.4

--- library-backend/index.js
+++ library-backend/index.js
@@ -114,23 +114,23 @@ const typeDefs = `
   }
 
   type Query {
-    bookCount(author: String): Int!
+    bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
 
 const resolvers = {
   Query: {
-    bookCount: (root, args) => {
-      if (args.author) {
-        return books.filter(book => book.author === args.author).length
-      }
-      return books.length
-    },
+    bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (args.author) {
+        return books.filter(book => book.author === args.author)
+      }
+      return books
+    },
     allAuthors: () => authors,
   },
   Author: {
