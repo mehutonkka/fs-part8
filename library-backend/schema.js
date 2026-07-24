6dc6e9c4df6be9bbcd7ba6a29c6a7f5639f6a541
Mishka Hagelberg <mishka.hagelberg@gmail.com>
2026-07-24 15:11:40 +0300

    4.16

--- library-backend/schema.js
+++ library-backend/schema.js
@@ -1,4 +1,14 @@
 const typeDefs = /* GraphQL */ `
+  type User {
+    username: String!
+    favoriteGenre: String!
+    id: ID!
+  }
+
+  type Token {
+    value: String!
+  }
+
   type Book {
     title: String!
     published: Int!
@@ -19,6 +29,7 @@ const typeDefs = /* GraphQL */ `
     authorCount: Int!
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
+    me: User
   }
 
   type Mutation {
@@ -34,6 +45,15 @@ const typeDefs = /* GraphQL */ `
       setBornTo: Int!
     ): Author
 
+    createUser(
+      username: String!
+      favoriteGenre: String!
+      ): User
+    
+    login(
+      username: String!
+      password: String!
+    ): Token
   }
 `
 module.exports = typeDefs
